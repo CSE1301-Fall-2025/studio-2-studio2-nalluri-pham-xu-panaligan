5b7fe3ec104801295fa78f699762c0b8d2a48338
Jimmy Xu <jiayangx@wustl.edu>
2025-09-10 15:39:23 -0500

    .

---
+++ src/Ruin.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Ruin {
+public static void main(String[]args){
+    Scanner in = new Scanner(System.in);
+System.out.println("What's your start amount?");
+double startAmount = in.nextDouble();
+System.out.println("What's your winning chance?");
+double winChance = in.nextDouble();
+System.out.println("What's your winning limit?");
+double winLimit = in.nextDouble();
+}
+while (startAmount>0 && ){
+Math.random()<
+}
+}
